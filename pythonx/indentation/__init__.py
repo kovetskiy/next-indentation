ae37c544f698f1b72b543c422ddf6449a8f324f4
Dmitry Khasanov <pik4ez@gmail.com>
2015-10-07 12:00:33 +0300

    Added spaced indentation support.

--- pythonx/indentation/__init__.py
+++ pythonx/indentation/__init__.py
@@ -1,18 +1,22 @@
 import _vim
 
+
 def go_down(count):
     return _go(1, count)
+
 
 def go_up(count):
     return _go(-1, count)
 
+
 def _go(direction, count):
     line_level = get_next_line_level(direction, count)
-    if line_level == None:
+    if line_level is None:
         return
 
     line, level = line_level
     _vim.goto(line, level)
+
 
 def get_next_line_level(direction, count):
     current_line = _vim.line_number()
@@ -39,9 +43,12 @@ def get_next_line_level(direction, count):
 
 def get_level(line_number):
     line = _vim.buffer()[line_number]
+
     column = 0
     for symbol in line:
-        if symbol == "\t":
+        if symbol == "\t" or symbol == " ":
             column += 1
+        else:
+            break
 
     return column
